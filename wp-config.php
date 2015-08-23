86d5dbd37626683ed8233a52e10d578e24a45c6c
carlosbvz <carlosbvz@hotmail.com>
2015-08-23 09:52:22 -0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@ define('DB_NAME', 'proifed');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Mysql2015');
+define('DB_PASSWORD', 'myPassword');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
